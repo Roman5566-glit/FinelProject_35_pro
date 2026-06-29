8e3961e228e0398a6ab73fe71882e5d365a23b82
Litvitskyi Roman <litvitskiyromka@gmail.com>
2026-06-29 15:35:52 +0300

    Fix

--- config/wsgi.py
+++ config/wsgi.py
@@ -8,9 +8,17 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
-
+from django.contrib.auth import get_user_model
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
+
+try:
+    User = get_user_model()
+    if not User.objects.filter(username='Admin').exists():
+        User.objects.create_superuser('Admin', 'admin@example.com', '0000')
+        print("Суперпользователь успешно создан!")
+except Exception as e:
+    print(f"Ошибка создания суперпользователя: {e}")
